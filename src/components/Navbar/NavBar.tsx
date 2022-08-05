c345fa0e2a7f06b6dbb682ec92c92e24b953df48
DanShortt <danieljpshortt@hotmail.com>
2022-08-05 18:20:42 +0000

    test

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className = "navBar">
         <h1 className = "navTitle">Payments Application</h1>
         <ul className="nav">
-            <li className = "navLink"><a href="#">find a transaction</a></li>
+            <li className = "navLink"><a href="#">find a Transaction</a></li>
             <li className = "navLink"><a href="#">New Transaction</a></li>
         </ul>
     </div>
